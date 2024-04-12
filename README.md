fd42b88b518ca773abd0a2ad253fbb343552f405
Olegs Kunicins <olegs.kunicins@gmail.com>
2024-04-12 08:01:03 +0300

    ES 8.13.1, 8.13.2

--- README.md
+++ README.md
@@ -6,6 +6,18 @@ The plugin tracks CRUD changes made in an ElasticSearch index.
 
 Target folder contains compiled binaries ready for use. The first 3 numbers of the version tag refers to the version of ElasticSearch the plugin built for.
 
+* ElasticSearch 8.13.2
+
+``` bash
+elasticsearch-plugin install https://github.com/OlegKunitsyn/elasticsearch-followup-plugin/raw/master/target/elasticsearch-followup-plugin-8.13.2.1.zip
+```
+
+* ElasticSearch 8.13.1
+
+``` bash
+elasticsearch-plugin install https://github.com/OlegKunitsyn/elasticsearch-followup-plugin/raw/master/target/elasticsearch-followup-plugin-8.13.1.1.zip
+```
+
 * ElasticSearch 8.13.0
 
 ``` bash
--- src/main/java/de/artcom_venture/elasticsearch/followup/FollowUpPlugin.java
+++ src/main/java/de/artcom_venture/elasticsearch/followup/FollowUpPlugin.java
@@ -2,10 +2,12 @@ package de.artcom_venture.elasticsearch.followup;
 
 import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
 import org.elasticsearch.cluster.node.DiscoveryNodes;
+import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
 import org.elasticsearch.common.settings.ClusterSettings;
 import org.elasticsearch.common.settings.IndexScopedSettings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.settings.SettingsFilter;
+import org.elasticsearch.features.NodeFeature;
 import org.elasticsearch.index.IndexModule;
 import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.rest.RestController;
@@ -13,9 +15,10 @@ import org.elasticsearch.rest.RestHandler;
 import org.elasticsearch.plugins.ActionPlugin;
 import static java.util.Collections.singletonList;
 
-import java.util.List;
+import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 /**
@@ -32,9 +35,7 @@ public class FollowUpPlugin extends Plugin implements ActionPlugin {
 	}
 	
 	@Override
-	public List<RestHandler> getRestHandlers(Settings settings, RestController restController,
-			ClusterSettings clusterSettings, IndexScopedSettings indexScopedSettings, SettingsFilter settingsFilter,
-			IndexNameExpressionResolver indexNameExpressionResolver, Supplier<DiscoveryNodes> nodesInCluster) {
+	public Collection<RestHandler> getRestHandlers(Settings settings, NamedWriteableRegistry namedWriteableRegistry, RestController restController, ClusterSettings clusterSettings, IndexScopedSettings indexScopedSettings, SettingsFilter settingsFilter, IndexNameExpressionResolver indexNameExpressionResolver, Supplier<DiscoveryNodes> nodesInCluster, Predicate<NodeFeature> clusterSupportsFeature) {
 		return singletonList(new FollowUpAction(settings, restController));
 	}
 	
